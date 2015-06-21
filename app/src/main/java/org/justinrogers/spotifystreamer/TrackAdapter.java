6d0d286f3217a763f21428ff9d689a52749e82f4
yaskyj <yaskyj@yahoo.com>
2015-06-21 11:28:06 -0500

    feat: Add album name and album image to track view
    Add image and album name to track view and adjust layout of individual tracks

--- app/src/main/java/org/justinrogers/spotifystreamer/TrackAdapter.java
+++ app/src/main/java/org/justinrogers/spotifystreamer/TrackAdapter.java
@@ -5,7 +5,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
@@ -32,7 +35,15 @@ public class TrackAdapter extends ArrayAdapter<Track> {
         Track track = getItem(position);
         TextView songNameView = (TextView) convertView.findViewById(R.id.list_song_name);
         songNameView.setText(track.name);
-
+        TextView albumNameView = (TextView) convertView.findViewById(R.id.list_album_name);
+        albumNameView.setText(track.album.name);
+        ImageView albumImageView = (ImageView) convertView.findViewById(R.id.list_track_image);
+        if (track.album.images.size() > 0) {
+            int littleThumbnailPos = track.album.images.size() - 1;
+            Picasso.with(getContext()).load(track.album.images.get(littleThumbnailPos).url).into(albumImageView);
+        } else {
+            Picasso.with(getContext()).load(R.drawable.abc_list_pressed_holo_dark).into(albumImageView);
+        }
         return convertView;
     }
 }
