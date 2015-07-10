cff6f963e0659a6ef880e974ab01978a8812ee0a
yaskyj <yaskyj@yahoo.com>
2015-07-10 11:26:23 -0500

    refactor: Use ButterKnife with viewHolder

--- app/src/main/java/org/justinrogers/spotifystreamer/ArtistAdapter.java
+++ app/src/main/java/org/justinrogers/spotifystreamer/ArtistAdapter.java
@@ -5,6 +5,7 @@
 package org.justinrogers.spotifystreamer;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,6 +17,8 @@ import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import kaaes.spotify.webapi.android.models.Artist;
 
 
@@ -26,25 +29,42 @@ public class ArtistAdapter extends ArrayAdapter<Artist> {
 
     private final String LOG_TAG = ArtistAdapter.class.getSimpleName();
 
+    public static class ViewHolder {
+        @Bind(R.id.list_artist_image)
+        ImageView artistImageView;
+        @Bind(R.id.list_artist_name)
+        TextView artistNameView;
+
+        public ViewHolder(View view) {
+            ButterKnife.bind(this, view);
+        }
+    }
     public ArtistAdapter(Context context, int resource, List<Artist> artistList) {
         super(context, resource, artistList);
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder viewHolder = null;
         if (convertView == null) {
             LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            convertView = inflater.inflate(R.layout.list_item_artists, parent,false);
+            convertView = inflater.inflate(R.layout.list_item_artists, parent, false);
+            viewHolder = new ViewHolder(convertView);
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
         }
         Artist artist = getItem(position);
-        TextView artistNameView = (TextView) convertView.findViewById(R.id.list_artist_name);
-        artistNameView.setText(artist.name);
-        ImageView artistImageView = (ImageView) convertView.findViewById(R.id.list_artist_image);
+        viewHolder.artistNameView.setText(artist.name);
+//        TextView artistNameView = (TextView) convertView.findViewById(R.id.list_artist_name);
+//        artistNameView.setText(artist.name);
+
+//        ImageView artistImageView = (ImageView) convertView.findViewById(R.id.list_artist_image);
         if (artist.images.size() > 0) {
             int littleThumbnailPos = artist.images.size() - 1;
-            Picasso.with(getContext()).load(artist.images.get(littleThumbnailPos).url).into(artistImageView);
+            Picasso.with(getContext()).load(artist.images.get(littleThumbnailPos).url).into(viewHolder.artistImageView);
         } else {
-            Picasso.with(getContext()).load(R.drawable.default_image_substitute).into(artistImageView);
+            Picasso.with(getContext()).load(R.drawable.default_image_substitute).into(viewHolder.artistImageView);
         }
 
         return convertView;
